4e1b2cc50f1c1cc251464530640d9402219a61ce
Warhorst <46809804+Warhorst@users.noreply.github.com>
2025-06-26 16:54:04 +0200

    Created a wfc result at the start of the playground which gets rendered in the terminal

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,37 +1,43 @@
 use color_eyre::Result;
-use std::io;
 use crossterm::event;
 use crossterm::event::{Event, KeyCode, KeyEventKind};
-use ratatui::DefaultTerminal;
+use pad::{p, Position};
 use ratatui::prelude::*;
 use ratatui::symbols::border;
 use ratatui::widgets::Block;
+use ratatui::DefaultTerminal;
+use std::io;
+use wave_function_collapse::{PossibleNeighbours, WaveFunctionCollapse};
+use Tile::*;
 
 fn main() -> Result<()> {
     color_eyre::install()?;
     let terminal = ratatui::init();
-    let run_result = Playground::default().run(terminal);
+
+    let mut playground = Playground {
+        stopped: false,
+        collapsed: collapse()
+    };
+
+    let run_result = playground.run(terminal);
     ratatui::restore();
     Ok(run_result?)
 }
 
 #[derive(Default)]
 struct Playground {
-    stopped: bool
+    stopped: bool,
+    collapsed: Vec<(Position, Tile)>
 }
 
 impl Playground {
     fn run(&mut self, mut terminal: DefaultTerminal) -> io::Result<()> {
         while !self.stopped {
-            terminal.draw(|frame| self.draw(frame))?;
+            terminal.draw(|frame| frame.render_widget(&*self, frame.area()))?;
             self.handle_events()?;
         }
 
         Ok(())
-    }
-
-    fn draw(&self, frame: &mut Frame) {
-        frame.render_widget(self, frame.area());
     }
 
     fn handle_events(&mut self) -> io::Result<()> {
@@ -58,6 +64,68 @@ impl Widget for &Playground {
             .title(title.centered())
             .border_set(border::THICK);
 
-        block.render(area, buf)
+        let inner = block.inner(area);
+
+        block.render(area, buf);
+
+        let mut sorted = self.collapsed.clone();
+        sorted.sort_by(|(pos_a, _), (pos_b, _)| pos_a.cmp(pos_b));
+
+        for (xi, x) in (inner.left()..inner.right()).enumerate() {
+            for (yi, y) in (inner.top()..inner.bottom()).enumerate() {
+                if let Some((_, tile)) = sorted.iter().find(|(pos, _)| *pos == p!(xi, yi)) {
+                    let char = tile.get_char();
+                    let color = tile.get_color();
+                    buf[layout::Position::new(x, y)].set_char(char).set_fg(color);
+                }
+            }
+        }
     }
+}
+
+#[derive(Copy, Clone, Eq, PartialEq)]
+enum Tile {
+    Water,
+    Sand,
+    Forest
+}
+
+impl Tile {
+    fn get_char(&self) -> char {
+        match self {
+            Water => 'W',
+            Sand => 'S',
+            Forest => 'F'
+        }
+    }
+
+    fn get_color(&self) -> Color {
+        match self {
+            Water => Color::Blue,
+            Sand => Color::Yellow,
+            Forest => Color::Green
+        }
+    }
+}
+
+fn collapse() -> Vec<(Position, Tile)> {
+    let tiles = vec![Water, Sand, Forest];
+    let possible_neighbours = PossibleNeighbours::new([
+          (Water, Water),
+          (Water, Sand),
+          (Sand, Water),
+          (Sand, Sand),
+          (Sand, Forest),
+          (Forest, Sand),
+          (Forest, Forest),
+    ], &tiles);
+
+    WaveFunctionCollapse::<3, Tile>::new(
+        20,
+        20,
+        tiles,
+    )
+        .with_constraint(possible_neighbours)
+        .with_seed(42)
+        .collapse()
 }
