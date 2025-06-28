fbacc637367d5d2be1e940d77c546bbbb362af42
Warhorst <46809804+Warhorst@users.noreply.github.com>
2025-06-28 19:19:33 +0200

    The control panel now uses a table. The table is part of the state.

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -4,10 +4,10 @@ use crossterm::event::{Event, KeyCode, KeyEventKind};
 use pad::{p, Position};
 use ratatui::buffer::Buffer;
 use ratatui::layout::{Constraint, Layout, Rect};
-use ratatui::style::Color;
+use ratatui::style::{Color, Style, Stylize};
 use ratatui::symbols::border;
 use ratatui::text::Line;
-use ratatui::widgets::{Block, List, StatefulWidget, Widget};
+use ratatui::widgets::{Block, Row, StatefulWidget, Table, TableState, Widget};
 use ratatui::{layout, DefaultTerminal};
 use std::io;
 use wave_function_collapse::{PossibleNeighbours, WaveFunctionCollapse};
@@ -17,16 +17,19 @@ fn main() -> Result<()> {
     color_eyre::install()?;
     let terminal = ratatui::init();
 
+    let mut ts = TableState::default();
+    ts.select(Some(0));
+
     let mut playground = Playground {
         state: State {
             stopped: false,
-            selected_screen: SelectedScreen::ControlScreen
+            selected_screen: SelectedScreen::ControlScreen,
+            table_state: ts
         },
         result_screen: ResultScreen {
             collapsed: collapse()
         },
         control_screen: ControlScreen {
-            setting_index: 0,
             width: 0,
             height: 0,
         }
@@ -37,12 +40,14 @@ fn main() -> Result<()> {
     Ok(run_result?)
 }
 
+// todo create sub states for the screens
 struct State {
     stopped: bool,
-    selected_screen: SelectedScreen
+    selected_screen: SelectedScreen,
+    table_state: TableState
 }
 
-#[derive(Eq, PartialEq)]
+#[derive(Clone, Copy, Eq, PartialEq)]
 enum SelectedScreen {
     ResultScreen,
     ControlScreen
@@ -67,16 +72,24 @@ impl Playground {
     fn handle_events(&mut self) -> io::Result<()> {
         match event::read()? {
             Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
-                match key_event.code {
-                    KeyCode::Char('q') => {
+                match (key_event.code, self.state.selected_screen) {
+                    (KeyCode::Char('q'), _) => {
                         self.state.stopped = true;
                     },
-                    KeyCode::Char('c') => {
+                    (KeyCode::Char('c'), SelectedScreen::ResultScreen) => {
+                        self.state.table_state.select(Some(0));
                         self.state.selected_screen = SelectedScreen::ControlScreen
                     },
-                    KeyCode::Char('r') => {
+                    (KeyCode::Char('r'), SelectedScreen::ControlScreen) => {
+                        self.state.table_state.select(None);
                         self.state.selected_screen = SelectedScreen::ResultScreen
-                    }
+                    },
+                    (KeyCode::Up, SelectedScreen::ControlScreen) => {
+                        self.state.table_state.scroll_up_by(1)
+                    },
+                    (KeyCode::Down, SelectedScreen::ControlScreen) => {
+                        self.state.table_state.scroll_down_by(1);
+                    },
                     _ => {}
                 }
             }
@@ -134,9 +147,9 @@ impl StatefulWidget for &ResultScreen {
     }
 }
 
+// todo move to super state
 struct ControlScreen {
     /// The index of the setting currently selected
-    setting_index: usize,
     width: usize,
     height: usize
 }
@@ -157,13 +170,19 @@ impl StatefulWidget for &ControlScreen {
 
         block.render(area, buf);
 
-        let list = List::new([
-            "Foo",
-            "Bar",
-            "Baz"
-        ]);
+        let rows = [
+            Row::new(vec!["Width".to_string(), format!("{}", self.width)]),
+            Row::new(vec!["Height".to_string(), format!("{}", self.height)])
+        ];
+        let widths = [
+            Constraint::Percentage(50),
+            Constraint::Percentage(50)
+        ];
+        let table = Table::new(rows, widths)
+            .row_highlight_style(Style::new().reversed())
+            .highlight_symbol(">");
 
-        Widget::render(list, inner, buf);
+        StatefulWidget::render(table, inner, buf, &mut state.table_state);
     }
 }
 
