2f83d4621e6a48f8fe9fe5bc6b75278feb7c8cf9
Warhorst <46809804+Warhorst@users.noreply.github.com>
2025-08-10 12:23:39 +0200

    Added benchmarks for multiple different dimensions in the wfc

--- benches/wfc_benchmark.rs
+++ benches/wfc_benchmark.rs
@@ -1,4 +1,4 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use wave_function_collapse::{constraints::PossibleNeighbours, WaveFunctionCollapse};
 use crate::Tile::*;
 
@@ -9,16 +9,16 @@ enum Tile {
     Forest
 }
 
-pub fn benchmark(c: &mut Criterion) {
-    c.bench_function("wfc", |b| b.iter(|| {
+pub fn simple(c: &mut Criterion) {
+    c.bench_function("simple", |b| b.iter(|| {
         let tiles = vec![Water, Sand, Forest];
         let possible_neighbours = PossibleNeighbours::new([
-            (Water, Water),
-            (Water, Sand),
-            (Sand, Water),
-            (Sand, Sand),
-            (Sand, Forest),
-            (Forest, Forest),
+              (Water, Water),
+              (Water, Sand),
+              (Sand, Water),
+              (Sand, Sand),
+              (Sand, Forest),
+              (Forest, Forest),
         ], &tiles);
 
         WaveFunctionCollapse::<3, Tile>::new(
@@ -32,5 +32,33 @@ pub fn benchmark(c: &mut Criterion) {
     }));
 }
 
-criterion_group!(benches, benchmark);
+pub fn multi_dimension(c: &mut Criterion) {
+    let dimensions = [50, 75, 100, 125];
+
+    for dim in dimensions {
+        c.bench_with_input(BenchmarkId::new("multi_dimension", dim), &dim, |b, dim| b.iter(|| {
+            let tiles = vec![Water, Sand, Forest];
+            let possible_neighbours = PossibleNeighbours::new([
+                  (Water, Water),
+                  (Water, Sand),
+                  (Sand, Water),
+                  (Sand, Sand),
+                  (Sand, Forest),
+                  (Forest, Forest),
+            ], &tiles);
+
+            WaveFunctionCollapse::<3, Tile>::new(
+                *dim,
+                *dim,
+                tiles
+            )
+                .with_constraint(possible_neighbours)
+                .with_seed(42)
+                .collapse();
+        }));
+    }
+
+}
+
+criterion_group!(benches, simple, multi_dimension);
 criterion_main!(benches);
