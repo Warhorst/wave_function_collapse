86146124cd54994d973014da4ea32d363a350800
Warhorst <46809804+Warhorst@users.noreply.github.com>
2026-01-19 08:20:35 +0100

    Implemented a version of the cell which uses a bitarray internally and no longer requires the const generic

--- src/cell.rs
+++ src/cell.rs
@@ -1,14 +1,20 @@
+use std::collections;
+
 use bitarray::BitArray;
 
 use crate::MAX_NUM_TILES;
 
-pub struct Cell_ {
+pub type Cell<const C: usize> = Cell_BA<C>;
+
+#[derive(Clone, Copy)]
+pub struct Cell_BA<const C: usize> {
     pub entropy: u8,
     max_entropy: u8,
     tile_indices: BitArray<u128>,
+    collapsed_index: Option<u8>,
 }
 
-impl Cell_ {
+impl<const C: usize> Cell_BA<C> {
     pub fn new(num_tiles: usize) -> Self {
         let mut tile_indices = BitArray::new(0);
 
@@ -16,46 +22,58 @@ impl Cell_ {
             tile_indices.set(i as u8, true);
         }
 
-        Cell_ {
+        Cell_BA {
             entropy: num_tiles as u8,
             max_entropy: num_tiles as u8,
             tile_indices,
+            collapsed_index: None,
         }
     }
 
-    // pub fn get_possible_indices(&self) -> &[u8] {
-    //     &self.tile_indices[0..self.entropy as usize]
-    // }
+    pub fn get_possible_indices(&self) -> PossibleIndices {
+        match self.collapsed_index {
+            Some(i) => PossibleIndices::new([i]),
+            None => PossibleIndices::new(self.tile_indices.ones().take(self.max_entropy as usize)),
+        }
+    }
 
-    // pub fn collapse(&mut self, index: u8) {
-    //     self.tile_indices[0] = index;
-    //     self.entropy = 1;
-    // }
+    pub fn collapse(
+        &mut self,
+        index: u8,
+    ) {
+        self.collapsed_index = Some(index);
+        self.entropy = 1;
+    }
 
-    // pub fn set_indices(&mut self, indices: impl IntoIterator<Item=u8>) {
-    //     let mut entropy = 0;
+    pub fn set_indices(
+        &mut self,
+        indices: impl IntoIterator<Item = u8>,
+    ) {
+        let mut entropy = 0;
+        let mut tile_indices = BitArray::new(0);
 
-    //     for (i, index) in indices.into_iter().enumerate() {
-    //         if index == u8::MAX {
-    //             // usize::MAX is used as a placeholder. The first occurrence
-    //             // of it tells that there are no more indices to consider
-    //             break;
-    //         }
+        for index in indices {
+            if index == u8::MAX {
+                // usize::MAX is used as a placeholder. The first occurrence
+                // of it tells that there are no more indices to consider
+                break;
+            }
 
-    //         self.tile_indices[i] = index;
-    //         entropy += 1;
-    //     }
+            tile_indices.set(index, true);
+            entropy += 1;
+        }
 
-    //     self.entropy = entropy
-    // }
+        self.entropy = entropy;
+        self.tile_indices = tile_indices
+    }
 
-    // pub fn get_collapsed_index(&self) -> u8 {
-    //     self.tile_indices[0]
-    // }
+    pub fn get_collapsed_index(&self) -> u8 {
+        self.collapsed_index.unwrap()
+    }
 
-    // pub fn is_collapsed(&self) -> bool {
-    //     self.entropy == 1
-    // }
+    pub fn is_collapsed(&self) -> bool {
+        self.entropy == 1
+    }
 }
 
 // TODO maybe provide a dynamic variant which works with vectors internally (Or a fixed array and the amount of tiles, an I just use slices)
@@ -63,14 +81,14 @@ impl Cell_ {
 /// The const generic C represents the maximum amount of tiles possible and therefore the capacity of
 /// the cell.
 #[derive(Clone, Copy)]
-pub struct Cell<const C: usize> {
+pub struct Cell_CG<const C: usize> {
     /// the current entropy of the cell, or the amount of still possible tiles
     pub entropy: u8,
     /// the indices of all tiles which are currently possible in this cell; only the entries until the self.entropy index are used
     tile_indices: [u8; C],
 }
 
-impl<const C: usize> Cell<C> {
+impl<const C: usize> Cell_CG<C> {
     pub fn new(num_tiles: usize) -> Self {
         let mut tile_indices = [0; C];
 
@@ -78,7 +96,7 @@ impl<const C: usize> Cell<C> {
             *index = i as u8
         }
 
-        Cell {
+        Cell_CG {
             entropy: num_tiles as u8,
             tile_indices,
         }
@@ -86,11 +104,7 @@ impl<const C: usize> Cell<C> {
 
     // TODO this should not return a slice. Maybe return an iter and create arrays where needed at the call side
     pub fn get_possible_indices(&self) -> PossibleIndices {
-        PossibleIndices::new(
-            self.tile_indices[0..self.entropy as usize]
-                .iter()
-                .copied(),
-        )
+        PossibleIndices::new(self.tile_indices[0..self.entropy as usize].iter().copied())
     }
 
     pub fn collapse(
@@ -167,17 +181,27 @@ impl PossibleIndices {
             .take_while(|i| *i != u8::MAX)
     }
 
-    pub fn get(&self, index: usize) -> u8 {
+    pub fn get(
+        &self,
+        index: usize,
+    ) -> u8 {
         // TODO bounds check and stuff
         self.possible_indices[index]
     }
 
-    pub fn push(&mut self, val: u8) {
+    pub fn push(
+        &mut self,
+        val: u8,
+    ) {
         self.possible_indices[self.entropy as usize] = val;
         self.entropy += 1
     }
 
-    pub fn set(&mut self, index: usize, val: u8) {
+    pub fn set(
+        &mut self,
+        index: usize,
+        val: u8,
+    ) {
         self.possible_indices[index] = val
     }
 
